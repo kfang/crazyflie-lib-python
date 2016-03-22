d628da3d0937235c98517492f9f48cb2473169db
kevin <kevin@zipfworks.com>
2016-03-22 15:43:03 -0700

    initial structure; needs testing

--- examples/ramp.py
+++ examples/ramp.py
@@ -30,9 +30,11 @@ the motors and disconnects.
 import logging
 import time
 from threading import Thread
+from threading import Timer
 
 import cflib
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -43,6 +45,11 @@ class MotorRampExample:
 
     def __init__(self, link_uri):
         """ Initialize and run the example with the specified link_uri """
+
+        self._nextThrottle = 0
+        self._stop = False
+        self._curr_baro = -1
+        self._target_baro = -1
 
         self._cf = Crazyflie()
 
@@ -63,6 +70,53 @@ class MotorRampExample:
         # Do not hijack the calling thread!
         Thread(target=self._ramp_motors).start()
 
+        # The definition of the logconfig can be made before connecting
+        self._lg_stab = LogConfig(name='Barometer', period_in_ms=500)
+        self._lg_stab.add_variable('baro.asl', 'float')
+        self._lg_stab.add_variable('baro.pressure', 'float')
+
+        # Adding the configuration cannot be done until a Crazyflie is
+        # connected, since we need to check that the variables we
+        # would like to log are in the TOC.
+        try:
+            self._cf.log.add_config(self._lg_stab)
+            # This callback will receive the data
+            self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
+            # This callback will be called on errors
+            self._lg_stab.error_cb.add_callback(self._stab_log_error)
+            # Start the logging
+            self._lg_stab.start()
+        except KeyError as e:
+            print('Could not start log configuration,'
+                  '{} not found in TOC'.format(str(e)))
+        except AttributeError:
+            print('Could not add Stabilizer log config, bad configuration.')
+
+    def _stop_cf(self):
+        self._stop = True
+
+    def _stab_log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs"""
+        print('Error when logging %s: %s' % (logconf.name, msg))
+        self._stop = True
+
+    def _stab_log_data(self, timestamp, data, logconf):
+        """Callback froma the log API when data arrives"""
+        print('[%s]: %s' % (self._target_baro, data['baro.asl']))
+        curr_baro = data['baro.asl']
+
+        if self._target_baro == -1:
+            self._target_baro = curr_baro + 1.0
+
+        if curr_baro < self._target_baro:
+            print("throttle up!")
+            if self._nextThrottle < 35000:
+                self._nextThrottle += 1500
+        elif curr_baro > self._target_baro:
+            print("throttle down!")
+            if self._nextThrottle > 0:
+                self._nextThrottle -= 1500
+
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
         at the specified address)"""
@@ -81,19 +135,33 @@ class MotorRampExample:
         thrust_mult = 1
         thrust_step = 500
         thrust = 20000
-        pitch = 0
+
+        pitch = 5
         roll = 0
         yawrate = 0
 
+        print('ramping motors...')
+
+        while self._nextThrottle == 0:
+            print('waiting for logging')
+            time.sleep(1.0)
+
         # Unlock startup thrust protection
         self._cf.commander.send_setpoint(0, 0, 0, 0)
+        print('unlocked thrust protection')
 
-        while thrust >= 20000:
-            self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+        while not self._stop:
+            print('throttle %s' % self._nextThrottle)
+            self._cf.commander.send_setpoint(roll, pitch, yawrate, self._nextThrottle)
             time.sleep(0.1)
-            if thrust >= 25000:
-                thrust_mult = -1
-            thrust += thrust_step * thrust_mult
+
+        # while thrust >= 20000:
+        #     self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+        #     time.sleep(0.1)
+        #     if thrust >= 25000:
+        #         thrust_mult = -1
+        #     thrust += thrust_step * thrust_mult
+
         self._cf.commander.send_setpoint(0, 0, 0, 0)
         # Make sure that the last packet leaves before the link is closed
         # since the message queue is not flushed before closing
