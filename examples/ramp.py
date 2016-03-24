09e76a673f9ba96f03dee5c3c07c2c5dbd68129d
kevin <kevin@zipfworks.com>
2016-03-24 15:34:09 -0700

    remove the do nothing

--- examples/ramp.py
+++ examples/ramp.py
@@ -30,11 +30,12 @@ the motors and disconnects.
 import logging
 import time
 from threading import Thread
-from threading import Timer
 
 import cflib
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
+
+import pygame as pg
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -50,6 +51,9 @@ class MotorRampExample:
         self._stop = False
         self._curr_baro = -1
         self._target_baro = -1
+        self._target_pitch = -1
+        self._pitch = 0
+        self._roll = 0
 
         self._cf = Crazyflie()
 
@@ -66,12 +70,14 @@ class MotorRampExample:
         """ This callback is called form the Crazyflie API when a Crazyflie
         has been connected and the TOCs have been downloaded."""
 
+        self._cf.commander.set_client_xmode(True)
+
         # Start a separate thread to do the motor test.
         # Do not hijack the calling thread!
         Thread(target=self._ramp_motors).start()
 
         # The definition of the logconfig can be made before connecting
-        self._lg_stab = LogConfig(name='Barometer', period_in_ms=500)
+        self._lg_stab = LogConfig(name='Barometer', period_in_ms=100)
         self._lg_stab.add_variable('baro.asl', 'float')
         self._lg_stab.add_variable('baro.pressure', 'float')
         self._lg_stab.add_variable('stabilizer.pitch', 'float')
@@ -106,20 +112,64 @@ class MotorRampExample:
 
     def _stab_log_data(self, timestamp, data, logconf):
         """Callback froma the log API when data arrives"""
-        print('[%s]: %s' % (self._target_baro, data['baro.asl']))
-        curr_baro = data['baro.asl']
+        # print('[%s]: %s' % (self._target_baro, data['baro.asl']))
+        # print('%s' % data)
 
-        if self._target_baro == -1:
-            self._target_baro = curr_baro + 1.0
+        #
+        # if curr_roll > 100:
+        #     self._stop = True
+        #
+        # if self._target_baro == -1:
+        #     self._target_baro = curr_baro + 1.0
+        #     self._nextThrottle = 20000
+        #
+        # if self._target_pitch == -1:
+        #     self._target_pitch = curr_pitch
+        #
+        # if curr_pitch < self._target_pitch:
+        #     print("pitch up!")
+        #     self._pitch += 6
+        # elif curr_pitch > self._target_pitch:
+        #     print("pitch down!")
+        #     self._pitch -= 6
+        #
+        # if curr_baro < self._target_baro:
+        #     if self._nextThrottle < 40000:
+        #         self._nextThrottle += thrust_inc
+        # elif curr_baro > self._target_baro:
+        #     if self._nextThrottle > 0:
+        #         self._nextThrottle -= thrust_inc
 
-        if curr_baro < self._target_baro:
-            print("throttle up!")
-            if self._nextThrottle < 35000:
-                self._nextThrottle += 1500
-        elif curr_baro > self._target_baro:
-            print("throttle down!")
-            if self._nextThrottle > 0:
-                self._nextThrottle -= 1500
+    def stop(self):
+        self._stop = True
+
+    def throttle_up(self):
+        if self._nextThrottle < 50000:
+            self._nextThrottle += 1000
+
+    def throttle_down(self):
+        if self._nextThrottle > 0:
+            self._nextThrottle -= 1000
+
+    def pitch_forward(self):
+        if self._pitch < 60:
+            self._pitch += 5
+
+    def pitch_back(self):
+        if self._pitch > -60:
+            self._pitch -= 5
+
+    def roll_right(self):
+        if self._roll < 60:
+            self._roll += 5
+
+    def roll_left(self):
+        if self._roll > -60:
+            self._roll -= 5
+
+    def reset_pitch_roll(self):
+        self._roll = 0
+        self._pitch = 0
 
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
@@ -136,17 +186,13 @@ class MotorRampExample:
         print('Disconnected from %s' % link_uri)
 
     def _ramp_motors(self):
-        thrust_mult = 1
-        thrust_step = 500
-        thrust = 20000
-
-        pitch = 5
-        roll = 0
+        pitch_trim = 5
+        roll_trim = 13
         yawrate = 0
 
         print('ramping motors...')
 
-        while self._nextThrottle == 0:
+        while self._nextThrottle == 0 and not self._stop:
             print('waiting for logging')
             time.sleep(1.0)
 
@@ -155,16 +201,8 @@ class MotorRampExample:
         print('unlocked thrust protection')
 
         while not self._stop:
-            # print('throttle %s' % self._nextThrottle)
-            # self._cf.commander.send_setpoint(roll, pitch, yawrate, self._nextThrottle)
+            self._cf.commander.send_setpoint(self._roll + roll_trim, self._pitch + pitch_trim, yawrate, self._nextThrottle)
             time.sleep(0.1)
-
-        # while thrust >= 20000:
-        #     self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
-        #     time.sleep(0.1)
-        #     if thrust >= 25000:
-        #         thrust_mult = -1
-        #     thrust += thrust_step * thrust_mult
 
         self._cf.commander.send_setpoint(0, 0, 0, 0)
         # Make sure that the last packet leaves before the link is closed
@@ -172,18 +210,78 @@ class MotorRampExample:
         time.sleep(0.1)
         self._cf.close_link()
 
+def initPygame(le):
+    pg.init()
+    pg.display.set_mode((640, 480))
+    done = False
+    curr_action = 'none'
+
+    try:
+
+        while not done:
+            pg.time.delay(50)
+
+            for event in pg.event.get():
+
+                if event.type == pg.KEYUP:
+                    print('set none')
+                    curr_action = 'none'
+                    le.reset_pitch_roll()
+                elif event.type == pg.KEYDOWN and event.key == 273:
+                    curr_action = 'up'
+                elif event.type == pg.KEYDOWN and event.key == 274:
+                    print('throttle down!')
+                    curr_action = 'down'
+                elif event.type == pg.KEYDOWN and event.key == 119:
+                    print('forward!')
+                    curr_action = 'forward'
+                elif event.type == pg.KEYDOWN and event.key == 115:
+                    print('back!')
+                    curr_action = 'back'
+                elif event.type == pg.KEYDOWN and event.key == 97:
+                    print('left!')
+                    curr_action = 'left'
+                elif event.type == pg.KEYDOWN and event.key == 100:
+                    print('left!')
+                    curr_action = 'right'
+                elif event.type == pg.KEYDOWN and event.key == 27:
+                    le.stop()
+                    quit()
+                elif event.type == pg.KEYDOWN:
+                    print event.key
+
+            if curr_action == 'up':
+                le.throttle_up()
+            elif curr_action == 'down':
+                le.throttle_down()
+            elif curr_action == 'forward':
+                le.pitch_forward()
+            elif curr_action == 'back':
+                le.pitch_back()
+            elif curr_action == 'left':
+                le.roll_left()
+            elif curr_action == 'right':
+                le.roll_right()
+
+    finally:
+        print('goodbye')
+
 
 if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
+
     # Scan for Crazyflies and use the first one found
     print('Scanning interfaces for Crazyflies...')
     available = cflib.crtp.scan_interfaces()
+
     print('Crazyflies found:')
     for i in available:
         print(i[0])
 
     if len(available) > 0:
         le = MotorRampExample(available[0][0])
+        initPygame(le)
+
     else:
         print('No Crazyflies found, cannot run example')
